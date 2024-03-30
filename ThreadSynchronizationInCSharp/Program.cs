c19bda2c9e016dead36d1c73d0beba992ab9dd0c
lijotech <lijotech@gmail.com>
2024-03-30 23:00:31 +0100

    code for Lock and Monitor functions

---
+++ ThreadSynchronizationInCSharp/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create new instances of Account for lock and Monitor examples
+        AccountLock accountLock = new AccountLock(1000);
+        AccountMonitor accountMonitor = new AccountMonitor(1000);
+
+        // Start the threads for lock example
+        Thread[] threadsLock = new Thread[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Thread t = new Thread(new ThreadStart(accountLock.DoTransactions));
+            threadsLock[i] = t;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            threadsLock[i].Start();
+        }
+
+        // Start the threads for Monitor example
+        Thread[] threadsMonitor = new Thread[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Thread t = new Thread(new ThreadStart(accountMonitor.DoTransactions));
+            threadsMonitor[i] = t;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            threadsMonitor[i].Start();
+        }
+    }
+}
+
+class AccountLock
+{
+    private Object thisLock = new Object();
+    int balance;
+    Random r = new Random();
+    public AccountLock(int initial)
+    {
+        balance = initial;
+    }
+    int Withdraw(int amount)
+    {
+        // This condition will never be true unless the lock statement
+        // is commented out:
+        if (balance < 0)
+        {
+            throw new Exception("Negative Balance");
+        }
+        // Comment out the next line to see the effect of leaving out 
+        // the lock keyword:
+        lock (thisLock)
+        {
+            if (balance >= amount)
+            {
+                Console.WriteLine("Balance before Withdrawal (Lock) : " + balance);
+                Console.WriteLine("Amount to Withdraw : -" + amount);
+                balance = balance - amount;
+                Console.WriteLine("Balance after Withdrawal : " + balance);
+                return amount;
+            }
+            else
+            {
+                return 0; // transaction rejected
+            }
+        }
+    }
+    public void DoTransactions()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Withdraw(r.Next(1, 100));
+        }
+    }
+}
+
+class AccountMonitor
+{
+    private Object thisLock = new Object();
+    int balance;
+    Random r = new Random();
+    public AccountMonitor(int initial)
+    {
+        balance = initial;
+    }
+    int Withdraw(int amount)
+    {
+        Monitor.Enter(thisLock);
+        try
+        {
+            if (balance < 0)
+            {
+                throw new Exception("Negative Balance");
+            }
+            if (balance >= amount)
+            {
+                Console.WriteLine("Balance before Withdrawal (Monitor) : " + balance);
+                Console.WriteLine("Amount to Withdraw : -" + amount);
+                balance = balance - amount;
+                Console.WriteLine("Balance after Withdrawal : " + balance);
+                return amount;
+            }
+            else
+            {
+                return 0; // transaction rejected
+            }
+        }
+        finally
+        {
+            Monitor.Exit(thisLock);
+        }
+    }
+    public void DoTransactions()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Withdraw(r.Next(1, 100));
+        }
+    }
+}
