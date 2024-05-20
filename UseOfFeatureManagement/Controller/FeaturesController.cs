c77db28a777849734d973db80fdb7e470a0d7105
lijotech <lijotech@gmail.com>
2024-05-20 09:08:27 +0200

    Adds Boolean Feature end point

---
+++ UseOfFeatureManagement/Controller/FeaturesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+
+namespace UseOfFeatureManagement.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public FeaturesController(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        // Endpoint for BooleanFilter
+        [HttpGet("BooleanFeature")]
+        public async Task<IActionResult> GetBooleanFeature()
+        {
+            if (await _featureManager.IsEnabledAsync("BooleanFeature"))
+            {
+                return Ok("Boolean feature is enabled");
+            }
+            else
+            {
+                return BadRequest("Boolean feature is disabled");
+            }
+        }
+    }
+}
---
+++ UseOfFeatureManagement/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace UseOfFeatureManagement
+{
+    public class Startup
+    {
+        public IConfiguration _configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddFeatureManagement();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
