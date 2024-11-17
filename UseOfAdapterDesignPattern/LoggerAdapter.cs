575205a936c24d2ad7eacb4bc7d99dd2d6331654
lijotech <lijotech@gmail.com>
2024-11-17 15:20:11 +0100

    Adapter design pattern

---
+++ UseOfAdapterDesignPattern/LoggerAdapter.cs
@@ -0,0 +1,21 @@
+﻿namespace UseOfAdapterDesignPattern
+{
+    public class LoggerAdapter : INewLogger
+    {
+        private readonly LegacyLogger _legacyLogger;
+        private readonly INewLogger _newLogger;
+
+        public LoggerAdapter(LegacyLogger legacyLogger, INewLogger newLogger)
+        {
+            _legacyLogger = legacyLogger;
+            _newLogger = newLogger;
+        }
+
+        public void Log(string message)
+        {
+            // Adapt the legacy logger to the new logger interface
+            _legacyLogger.LogMessage(message);
+            _newLogger.Log(message);
+        }
+    }
+}
---
+++ UseOfAdapterDesignPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace UseOfAdapterDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using the legacy logger directly
+            var legacyLogger = new LegacyLogger();
+            var databaseLogger = new DatabaseLogger();
+            legacyLogger.LogMessage("This is a legacy log message.");
+
+            // Using both old and new approach with logger interface with the adapter
+            INewLogger newLogger = new LoggerAdapter(legacyLogger, databaseLogger);
+            newLogger.Log("This is a log message through the adapter.");
+        }
+    }
+
+    public class LegacyLogger
+    {
+        public void LogMessage(string message)
+        {
+            // Simulate writing to a text file
+            Console.WriteLine($"Logging to text file: {message}");
+        }
+    }
+
+    public interface INewLogger
+    {
+        void Log(string message);
+    }
+
+    public class DatabaseLogger : INewLogger
+    {
+        public void Log(string message)
+        {
+            // Simulate writing to a database
+            Console.WriteLine($"Logging to database: {message}");
+        }
+    }
+
+}
